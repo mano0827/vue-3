6ac7f1e2b671d7f26dda069dd3214ee6b400d050
mano0827 <au.dugout@gmail.com>
2021-07-16 10:28:54 +0900

    vue3課題

--- myvue3/src/components/HelloWorld.vue
+++ myvue3/src/components/HelloWorld.vue
@@ -1,58 +1,112 @@
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+    <body>
+      <h1>ToDoリスト</h1>
+      <label
+        ><input
+          type="radio"
+          name="list"
+          value="all"
+          v-model="filter"
+        />すべて</label
+      >
+      <label
+        ><input
+          type="radio"
+          name="list"
+          value="active"
+          v-model="filter"
+        />作業中</label
+      >
+      <label
+        ><input
+          type="radio"
+          name="list"
+          value="completed"
+          v-model="filter"
+        />完了</label
+      >
+
+      <table>
+        <tr v-for="(todo, key) in filteredTodos" :key="key">
+          <td>{{ todo.id }}</td>
+          <td>{{ todo.title }}</td>
+          <td>
+            <button @click="statusChange(todo.id)">
+              <span>{{ todo.statusBtn }}</span>
+            </button>
+          </td>
+          <td>
+            <button @click="deleteTask(todo.id)">{{ todo.delBtn }}</button>
+          </td>
+        </tr>
+      </table>
+
+      <h2>新規タスクの追加</h2>
+      <form @submit.prevent="addTask">
+        <input type="text" v-model="newTask" />
+        <input type="submit" value="追加" class="button" />
+      </form>
+    </body>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  data() {
+    return {
+      id: 0,
+      statusBtn: "",
+      newTask: "",
+      todos: [],
+      filter: "all",
+    };
+  },
+  computed: {
+    filteredTodos() {
+      if (this.filter === "all") {
+        return this.todos;
+      } else if (this.filter === "completed") {
+        return this.todos.filter((todo) => {
+          return todo.statusBtn === "完了";
+        });
+      } else if (this.filter === "active") {
+        return this.todos.filter((todo) => {
+          return todo.statusBtn === "作業中";
+        })
+      } 
+      else{
+        return this.todo.statusBtn === "";
+      }
+    },
+    },
+    methods: {
+      addTask: function () {
+        let item = {
+          id: this.id++,
+          title: this.newTask,
+          statusBtn: "作業中",
+          delBtn: "削除",
+        };
+        this.todos.push(item);
+        this.newTask = "";
+      },
+      statusChange: function (id) {
+        for (let todo of this.todos) {
+          if (todo.id === id) {
+            if (todo.statusBtn === "作業中") {
+              todo.statusBtn = "完了";
+            } else {
+              todo.statusBtn = "作業中";
+            }
+            break;
+          }
+        }
+      },
+      deleteTask: function (index) {
+        this.todos.splice(index, 1);
+      },
+    },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
